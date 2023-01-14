6a984d18804815fe668ebc376addd11cbea9a71d
voidoperator <julionunez@me.com>
2023-01-13 22:49:27 -0500

    feat: implement re-rucks initial file structuring

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,77 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, NavLink, useRouteMatch,
+} from 'react-router-dom';
+import ROUTES from './routes';
 
-function App() {
-  return <h1>leelo.</h1>;
+export default function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to={ROUTES.mejor()} activeClassName="active">
+              mejor
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={ROUTES.popular()} activeClassName="active">
+              popular
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={ROUTES.nuevo()} activeClassName="active">
+              nuevo
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      {/* <Switch>
+        <Route path="/topics">
+          <TopicsRoutes />
+        </Route>
+        <Route path="/quizzes">
+          <QuizRoutes />
+        </Route>
+      </Switch> */}
+    </Router>
+  );
 }
 
-export default App;
+// function TopicsRoutes() {
+//   const match = useRouteMatch();
+
+//   return (
+//     <Switch>
+//       <Route path={`${match.path}/new`}>
+//         <NewTopicForm />
+//       </Route>
+//       <Route path={`${match.path}/:topicId`}>
+//         <Topic />
+//       </Route>
+//       <Route path={`${match.path}`}>
+//         <Topics />
+//       </Route>
+//     </Switch>
+//   );
+// }
+
+// function QuizRoutes() {
+//   const match = useRouteMatch();
+
+//   return (
+//     <Switch>
+//       <Route path={`${match.path}/new`}>
+//         <NewQuizForm />
+//       </Route>
+//       <Route path={`${match.path}/:quizId`}>
+//         <Quiz />
+//       </Route>
+//       <Route path={`${match.path}`}>
+//         <Quizzes />
+//       </Route>
+//     </Switch>
+//   );
+// }
---
+++ src/app/routes.js
@@ -0,0 +1,8 @@
+const ROUTES = {
+  postRoute: (id) => `/publicacion/${id}`,
+  mejor: () => '/mejor',
+  popular: () => '/popular',
+  nuevo: () => '/nuevo',
+};
+
+export default ROUTES;
